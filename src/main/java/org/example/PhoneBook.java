2dd0c013ca150dffc079d02ad18243c4d20c13c8
Denis Chetvertkov <chetvertkov-denis96@mail.ru>
2023-09-19 23:15:28 +0300

    Stub and tests for findByNumber

--- src/main/java/org/example/PhoneBook.java
+++ src/main/java/org/example/PhoneBook.java
@@ -5,15 +5,17 @@ import java.util.Map;
 
 public class PhoneBook {
 
-    private Map <String, Integer> contacts = new HashMap<>();
-    public int add (String name, int TelNumber){
+    private Map <String, String> contacts = new HashMap<>();
+    public int add (String name, String TelNumber){
         if(!contacts.containsKey(name)){
             contacts.put(name, TelNumber);
         }
         return contacts.size();
     }
 
-
+    public String findByNumber(String TelNumber){
+        return null;
+    }
 
 
 }
--- src/test/java/PhoneBookTest.java
+++ src/test/java/PhoneBookTest.java
@@ -2,12 +2,24 @@ import org.example.PhoneBook;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 public class PhoneBookTest {
     @Test
     public void testAdd(){
         PhoneBook phoneBook = new PhoneBook();
-        int contactsCount = phoneBook.add("Alex", +795842457);
+        int contactsCount = phoneBook.add("Alex", "+795842457");
         assertEquals(1, contactsCount);
     }
+
+
+    @Test
+    public void testFindByNumber(){
+        PhoneBook phoneBook = new PhoneBook();
+        phoneBook.add("Alice", "123456789");
+        String name = phoneBook.findByNumber("123456789");
+        assertEquals("Alice", name);
+    }
+
+
 }
