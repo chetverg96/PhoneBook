51156559831f793c85d26cdcd124dd913303a2b9
Denis Chetvertkov <chetvertkov-denis96@mail.ru>
2023-09-19 22:56:25 +0300

    Stub and tests for add

--- src/main/java/org/example/PhoneBook.java
+++ src/main/java/org/example/PhoneBook.java
@@ -1,4 +1,15 @@
 package org.example;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class PhoneBook {
+
+    public int add (String name, int TelNumber){
+        return 0;
+    }
+
+
+
+
 }
--- src/test/java/PhoneBookTest.java
+++ src/test/java/PhoneBookTest.java
@@ -1,2 +1,13 @@
+import org.example.PhoneBook;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class PhoneBookTest {
+    @Test
+    public void testAdd(){
+        PhoneBook phoneBook = new PhoneBook();
+        int contactsCount = phoneBook.add("Alex", +795842457);
+        assertEquals(1, contactsCount);
+    }
 }
